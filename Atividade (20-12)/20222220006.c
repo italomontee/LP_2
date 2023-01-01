b83a419b9f3c14d164fa44341045352455a2f9e5
italomontee <120536529+italomontee@users.noreply.github.com>
2023-01-01 16:00:47 -0400

    Add files via upload

---
+++ Atividade (20-12)/20222220006.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(){
+	int i, n_1;
+	for (i = 0; i <= 10; i ++){
+		n_1 = i * 1;
+		printf("1 X %d igual a %d\n", i, n_1);
+	}
+	printf("\n");
+	for (i = 0; i <= 10; i ++){
+		n_1 = i * 2;
+		printf("2 X %d igual a %d\n", i, n_1);
+	}
+	printf("\n");
+	for (i = 0; i <= 10; i ++){
+		n_1 = i * 3;
+		printf("3 X %d igual a %d\n", i, n_1);
+	}
+	printf("\n");
+	for (i = 0; i <= 9; i ++){
+		n_1 = i * 4;
+		printf("4 X %d igual a %d\n", i, n_1);
+	}
+	printf("\n");
+	for (i = 0; i <= 9; i ++){
+		n_1 = i * 5;
+		printf("5 X %d igual a %d\n", i, n_1);
+	}
+	printf("\n");
+	for (i = 0; i <= 9; i ++){
+		n_1 = i * 6;
+		printf("6 X %d igual a %d\n", i, n_1);
+	}
+	printf("\n");
+	for (i = 0; i <= 9; i ++){
+		n_1 = i * 7;
+		printf("7 X %d igual a %d\n", i, n_1);
+	}
+	printf("\n");
+	for (i = 0; i <= 9; i ++){
+		n_1 = i * 8;
+		printf("8 X %d igual a %d\n", i, n_1);
+	}
+	printf("\n");
+	for (i = 0; i <= 9; i ++){
+		n_1 = i * 9;
+		printf("9 X %d igual a %d\n", i, n_1);
+	}
+}
