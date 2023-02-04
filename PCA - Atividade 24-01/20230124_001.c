0390249a746dfa222d9e9ef46d59e89e3f15f0a5
italomontee <120536529+italomontee@users.noreply.github.com>
2023-02-04 12:30:28 -0400

    Add files via upload

---
+++ PCA - Atividade 24-01/20230124_001.c
@@ -0,0 +1,2 @@
+/*  Criar um programa que  dada uma sequência de n números a imprima na ordem inversa à da leitura. (o comprimento máximo da sequência é 100 pelo que n tem que ser um número menor que 100)*/
+
