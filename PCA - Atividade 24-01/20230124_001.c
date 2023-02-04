34f6d947b9948df8b32b172782a833301ac318fb
italomontee <120536529+italomontee@users.noreply.github.com>
2023-02-04 12:56:54 -0400

    Update 20230124_001.c

--- PCA - Atividade 24-01/20230124_001.c
+++ PCA - Atividade 24-01/20230124_001.c
@@ -1,2 +1,23 @@
 /*  Criar um programa que  dada uma sequência de n números a imprima na ordem inversa à da leitura. (o comprimento máximo da sequência é 100 pelo que n tem que ser um número menor que 100)*/
+#include <stdio.h>
+#define LIM 101
 
+int main() {
+    int n, i;
+    int numeros[LIM];
+
+    printf("Digite o tamanho da sequencia: ");
+    scanf("%d", &n);
+
+    printf("entre com %d elementos", n);
+    for (i = 0; i<n; i++){
+        printf("\n%d:", i);
+        scanf("%d", &numeros[i]);
+    }
+    printf("Ordem inversa: ");
+    for ( i = n-1; i >= 0; i--){
+      printf("%d", numeros[i])  ;
+    }
+    printf("\n");
+    return 0;
+}
