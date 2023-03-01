a81a2bd48386d03d3b2c314f89f8de055961146f
italomontee <120536529+italomontee@users.noreply.github.com>
2023-03-01 00:18:24 -0400

    Update 20232802_AP1.c

--- 20232802_AP1/20232802_AP1.c
+++ 20232802_AP1/20232802_AP1.c
@@ -15,14 +15,37 @@ void adicionar_ponto(int notas[], char sexos[]) {
     }
 }
 
+void adicionar_aluno(char equipe[][20], int notas_eqp[], char sexo_eqp[]) {
+    int nota;
+    char nome[20];
+    char sexo;
+
+    
+    printf("\nnome: ");
+    scanf("%s", nome);
+    printf("sexo: ");
+    scanf(" %c", &sexo);
+    printf("nota: ");
+    scanf("%d", &nota);
+
+    
+    strcpy(equipe[6], nome);
+    sexo_eqp[6] = sexo;
+    notas_eqp[12] = nota;
+    
+    
+    
+}
+
 int main() {
-    char equipe1[6][20] = {"julia", "rebeca", "marcos", "joao", "ana", "maria"};
-    char equipe2[6][20] = {"mariana", "marta", "marcela", "camila", "jhony", "marcos"};
-    int notas[12] = {10, 6, 3, 2, 8, 8, 9, 10, 8, 8, 10, 5};
-    char sexos[12] = {'f','f', 'm', 'm', 'f', 'f', 'f', 'f', 'f', 'f', 'm', 'm'};
+    int tam = 100;
+    char equipe1[100][20] = {"julia", "rebeca", "marcos", "joao", "ana", "maria"};
+    char equipe2[100][20] = {"cuevas", "marta", "marcela", "camila", "jhony", "marcos"};
+    int notas[100] = {10, 6, 3, 2, 8, 8, 9, 10, 8, 8, 10, 5};
+    char sexos[100] = {'m','f', 'm', 'm', 'f', 'f', 'f', 'f', 'f', 'f', 'm', 'm'};
     char nome_projeto[10][20]; 
     
-    int i, j = 0, contagem_mulheres_equipe1 = 0, contagem_mulheres_equipe2 = 0, k = 0;
+    int i, j = 0, contagem_mulheres_equipe1 = 0, contagem_mulheres_equipe2 = 0, k = 0, count1=0, count2=0;
     
     for(i = 0; i < 6; i++) {
         if(sexos[i] == 'f') {
@@ -38,8 +61,14 @@ int main() {
     
     if(contagem_mulheres_equipe1 > contagem_mulheres_equipe2) {
         printf("A equipe 1 tem mais mulheres \n");
-    } else if(contagem_mulheres_equipe2 > contagem_mulheres_equipe1) {
+        }
+
+    if(contagem_mulheres_equipe2 > contagem_mulheres_equipe1) {
         printf("A equipe 2 tem mais mulheres \n");
+    }
+
+    else{
+        printf("Quantidades iguais de mulheres \n");
     }
     
     for(i = 0; i < 6; i++) {
@@ -62,6 +91,22 @@ int main() {
     }
     
     adicionar_ponto(notas, sexos);
-    
+    adicionar_aluno(equipe1, notas, sexos);
+        
+    printf("\n* Lista Atualizada *");
+
+
+    for(i = 0; i < 13; i++) {
+            printf("%s ", equipe1[i]);
+        }
+    printf("\n");
+    for(i = 0; i < 13; i++) {
+            printf("%d ", notas[i]);
+        }
+    printf("\n");
+    for(i = 0; i < 13; i++) {
+            printf("%c ", sexos[i]);
+        }
+
     return 0;
 }
