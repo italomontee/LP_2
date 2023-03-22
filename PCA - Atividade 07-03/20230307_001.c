f7f875694a9d18e2f18d38d596ee528d7f27e146
italomontee <120536529+italomontee@users.noreply.github.com>
2023-03-22 18:54:21 -0400

    Update 20230307_001.c

--- PCA - Atividade 07-03/20230307_001.c
+++ PCA - Atividade 07-03/20230307_001.c
@@ -5,56 +5,70 @@
 
 #include <stdio.h>
 int i;
-void imprimir(float *ptr1, float *ptr2, int tamanho){
-    for ( i = 0; i < tamanho; i++)
+int imprimir(float *a, float *b){
+    
+    for ( i = 0; i < b-a; i++)
     {
-        printf("%0.2f \n4", *(ptr1+i));
+        printf("%0.2f\n", *(a+i));
     }
     
-    
+
 }
-int main() {
-    int t1, t2, t3;
-    int i1, i2, i3, f1, f2, f3;
 
-    printf("Qual o tamanho do primeiro vetor? ");
+int main(){
+    int i1, f1, i2, f2, i3, f3;
+    int t1,t2,t3;
+    printf("Qual o tamanho do primeiro vetor: ");
     scanf("%d", &t1);
+
     float v1[t1];
-    for (i = 0; i < t1; i++) {
-        printf("Qual o %d valor do primeiro vetor? ", i + 1);
-        scanf("%f", &v1[i]);
+
+    for (i = 0; i < t1; i++)
+    {
+       printf("Elemento %i do v1: ");
+       scanf("%f", &v1[i]);
     }
-    printf("qual a posicao do vetor deve ser o inicio? ");
+    
+    printf("Qual posicao vai ser o inicio: ");
     scanf("%d", &i1);
-    printf("e final? ");
+    
+    printf("Qual posicao vai ser o final: ");
     scanf("%d", &f1);
-    imprimir(&v1[i1], &v1[f1], f1 - i1 + 1);
 
-    printf("Qual o tamanho do segundo vetor? ");
+    imprimir(&v1[i1], &v1[f1]+1);
+
+    printf("Qual o tamanho do segundo vetor: ");
     scanf("%d", &t2);
-    float v2[t2];
-    for (i = 0; i < t2; i++) {
-        printf("Qual o %d valor do segundo vetor? ", i + 1);
-        scanf("%f", &v2[i]);
-    }
-    printf("qual a posicao do vetor deve ser o inicio? ");
-    scanf("%d", &i2);
-    printf("e final? ");
-    scanf("%d", &f2);
-    imprimir(&v2[i2], &v2[f2], f2 - i2 + 1);
 
-    printf("Qual o tamanho do terceiro vetor? ");
-    scanf("%d", &t3);
-    float v3[t3];
-    for (i = 0; i < t3; i++) {
-        printf("Qual o %d valor do terceiro vetor? ", i + 1);
-        scanf("%f", &v3[i]);
+    float v2[t2];
+
+    for (i = 0; i < t2; i++)
+    {
+       printf("Elemento %i do v2: ");
+       scanf("%f", &v2[i]);
     }
-    printf("qual a posicao do vetor deve ser o inicio? ");
+    
+    printf("Qual posicao vai ser o inicio: ");
+    scanf("%d", &i2);
+    
+    printf("Qual posicao vai ser o final: ");
+    scanf("%d", &f2);
+
+    imprimir(&v2[i2], &v2[f2]+1);
+
+    float v3[t3];
+
+    for (i = 0; i < t3; i++)
+    {
+       printf("Elemento %i do v2: ");
+       scanf("%f", &v3[i]);
+    }
+    
+    printf("Qual posicao vai ser o inicio: ");
     scanf("%d", &i3);
-    printf("e final? ");
+    
+    printf("Qual posicao vai ser o final: ");
     scanf("%d", &f3);
 
-    imprimir(&v3[i3], &v3[f3], f3 - i3 + 1);
-
+    imprimir(&v3[i3], &v3[f3]+1);
 }
